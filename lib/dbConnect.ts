8b33e63b97e7df4a2629724c124f60aa47c35c21
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2025-06-26 21:52:04 +0530

    Update dbConnect.ts

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,10 +1,27 @@
-import mongoose from 'mongoose'
+// lib/mongodb.ts
+import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI!
-if (!MONGODB_URI) throw new Error('Missing MONGODB_URI')
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable inside .env.local or Netlify');
+}
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 async function dbConnect() {
-  if (mongoose.connection.readyState >= 1) return
-  await mongoose.connect(MONGODB_URI, { dbName: 'expensexpro' })
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI!, {
+      bufferCommands: false,
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
-export default dbConnect
+
+(global as any).mongoose = cached;
+
+export default dbConnect;
