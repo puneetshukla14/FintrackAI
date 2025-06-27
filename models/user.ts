7ff6441a2a6e91a40b412e1d935fd819292a1dd8
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2025-06-27 14:05:07 +0530

    Update user.ts

--- models/user.ts
+++ models/user.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true }
 })
 
 export default mongoose.models.User || mongoose.model('User', userSchema)
