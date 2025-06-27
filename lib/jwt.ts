a292319c7ff87a4e6bf014e727df765d5a60dfab
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2025-06-27 10:36:55 +0530

    Update jwt.ts

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -1,15 +1,11 @@
 import jwt from 'jsonwebtoken'
 
 export function signToken(payload: object) {
-  if (!process.env.JWT_SECRET) {
-    throw new Error('JWT_SECRET is not defined')
-  }
+  if (!process.env.JWT_SECRET) throw new Error('JWT_SECRET is not defined')
   return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' })
 }
 
 export function verifyToken(token: string) {
-  if (!process.env.JWT_SECRET) {
-    throw new Error('JWT_SECRET is not defined')
-  }
+  if (!process.env.JWT_SECRET) throw new Error('JWT_SECRET is not defined')
   return jwt.verify(token, process.env.JWT_SECRET)
 }
