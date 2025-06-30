7a6acf234612c6420cf1ed81892b019afd8a109e
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2025-06-30 11:13:13 +0530

    Update items.ts

--- lib/items.ts
+++ lib/items.ts
@@ -1,1 +1,8 @@
 
+export const items = [
+  { name: 'Redmi Buds 4 Active', price: 1199, emiOptions: [3, 6] },
+  { name: 'boAt Airdopes 161', price: 999, emiOptions: [] },
+  { name: 'Redmi 12 5G', price: 10999, emiOptions: [6, 12] },
+  { name: 'Amazon Prime 1 Yr', price: 1499, emiOptions: [] },
+  { name: 'Samsung Galaxy M14', price: 13499, emiOptions: [3, 6, 12] },
+]
